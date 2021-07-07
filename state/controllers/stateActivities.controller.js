f96fb21359bf7869ed8711602160f1ef5b6d5cda
William <william.chandra@student.umn.ac.id>
2021-07-07 14:43:03 +0700

    feat: create input into google cloud storage feature

--- state/controllers/stateActivities.controller.js
+++ state/controllers/stateActivities.controller.js
@@ -1,10 +1,17 @@
 const stateActivities = require('../models/stateActivities.model');
+const fs = require('fs');
+const helper = require('../../helpers/helper');
+
+//Google Cloud Storage Library and Keys
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage({
+    keyFilename: './keys/maxima-umn-2021-bucket-playground-key.json'
+});
 
 exports.getStateData = async (req, res)=>{
-    const { nim, stateID} = req.query;
+    const {nim, stateID} = req.query;
 
     try{
-        
         const result = await stateActivities.query();
         return res.status(200).send(result);
     }
@@ -35,19 +42,28 @@ exports.getStatebyParam = async (req, res)=>{
 
 exports.addState = async (req, res)=>{
     const {name, zoomLink, day, quota, attendanceCode} = req.body;
-
     const {stateLogo} = req.files;
 
-    const uploadPath = './stateLogo/' + name + '_' + stateLogo.name;
+    const dateFile = (helper.createAttendanceTime().split(' ')[0].split('-').join(''))
+    const timeFile = (helper.createAttendanceTime().split(' ')[1].split(':').join(''))
+       
+    //format filename = nama state + nama file + datetime upload file
+    const fileName = name + "_" + dateFile.concat(timeFile) + "_" +  stateLogo.name;
+   
+    const uploadPath = './stateLogo/' + fileName;
+
+    const bucket_name = "mxm21-bucket-playground";
+
+    const url_file = `https://storage.googleapis.com/${bucket_name}/${fileName}`;
 
     try{
         const insertResult = await stateActivities.query().insert({
-            name,
+            name, 
             zoomLink,
             day,
-            stateLogo: `${name}_${stateLogo.name}`,
+            stateLogo: url_file,
             quota,
-            registered: 0,
+            registered: 0, 
             attendanceCode
         });
 
@@ -55,10 +71,16 @@ exports.addState = async (req, res)=>{
             if(err) return res.status(500).send({message: err.messsage});
         })
 
-        return res.status(200).send({
+        res_bucket = await storage.bucket(bucket_name).upload(uploadPath);
+        
+        res.status(200).send({
             message: "Data berhasil ditambahkan",
-            data: insertResult
+            data: insertResult, 
         });
+
+        fs.unlink(uploadPath, (err)=>{
+            if(err) return res.status(500).send({message: err.messsage});
+        })
     }
     catch(err){
         return res.status(500).send({message: err.message});
